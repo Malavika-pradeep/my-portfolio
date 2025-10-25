ca5bcf8a1b8d3945c8120c02bba09d5ffa233cae
Malavika Pradeep <malavikapradeep2001@gmail.com>
2025-10-25 23:16:08 +0530

    Updates

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,9 @@
+
+import { useEffect, useState } from "react";
+
+export const StarBackground = () => {
+  return (
+    <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
+    </div>
+  );
+};
